1d4a8b972d13dfd559b280aff6aaff887cfc350b
Chaowei Zeng <sli616@163.com>
2019-07-12 10:34:43 +0800

    fix test.

--- src/test/java/UnitTestBasic.java
+++ src/test/java/UnitTestBasic.java
@@ -1,17 +1,27 @@
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-@RunWith(JUnit4.class)
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class UnitTestBasic {
     private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
     @Before
     public void setUp() {
         System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void should_capture_system_out_string() {
+        System.out.print("system out");
+
+        assertThat(systemOut(), is("system out"));
     }
 
     String systemOut() {
